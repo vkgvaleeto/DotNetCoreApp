7ed7eba47a750833318e0b09c44712ac65d01699
vkgvaleeto <70820630+vkgvaleeto@users.noreply.github.com>
2021-08-29 19:07:29 +0530

    first commit

---
+++ SRC/DotNetCoreApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCoreApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        public WeatherForecastController()
+        {
+           //
+        }
+
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "Vivek Gupta";
+        }
+    }
+}
---
+++ Test/DotNetCoreApp.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using DotNetCoreApp.Controllers;
+
+namespace DotNetCoreApp.Tests
+{
+    public class UnitTest1
+    {
+        WeatherForecastController obj = new  WeatherForecastController();
+        [Fact]
+        public void Test1()
+        {
+            var result = obj.Get(101);
+            Assert.Equal("Vivek Gupta",result);
+
+            
+        }
+    }
+}
