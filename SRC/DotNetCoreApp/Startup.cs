7d32ca3b979ee5d34e87ab72df428815dafe1d55
vkgvaleeto <70820630+vkgvaleeto@users.noreply.github.com>
2021-09-12 18:23:46 +0530

    phase2

--- SRC/DotNetCoreApp/Startup.cs
+++ SRC/DotNetCoreApp/Startup.cs
@@ -44,7 +44,7 @@ namespace DotNetCoreApp
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DotNetCoreApp v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
